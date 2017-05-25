c759cc7564b0b98cdc2e5b0a5db5bea489c6555c
김유빈 <1rladbqls@gmail.com>
2017-05-26 00:19:37 +0900

    배열 삭제

--- kimtest/src/SqDrawEx.java
+++ kimtest/src/SqDrawEx.java
@@ -5,9 +5,8 @@ import java.util.Vector;
 
 public class SqDrawEx extends JFrame 
 {
-	public static final int BOXMAX = 100;
-	private Rectangle[] box = new Rectangle[100];
-	public int k = 0;
+	public boolean moveSq = false;
+	public int v=0;//벡터 값 저장
 
 	public SqDrawEx()
 	{
@@ -22,9 +21,10 @@ public class SqDrawEx extends JFrame
 		
 		Point startP=null;
 		Point endP=null;
+		Point tempP=null;
 		
-		Vector<Point> sv = new Vector<Point>(); // 쩍횄�횤
-		Vector<Point> se = new Vector<Point>(); // 쨀징횁징
+		Vector<Point> starV = new Vector<Point>(); // 시작점
+		Vector<Point> endV = new Vector<Point>(); // 끝점
 
 		public MyPanel()
 		{
@@ -35,24 +35,19 @@ public class SqDrawEx extends JFrame
 			this.addMouseMotionListener(ml);
 		}
 		
+
+
 		public void paintComponent(Graphics g){
-			super.paintComponent(g); // 쨘횓쨍챨 횈채�횓횈짰횊짙횄창
-
-			
-
-			g.drawRect(box[k].x,box[k].y,box[k].width,box[k].height);	
-
-			
+			super.paintComponent(g); 			
 
 
-			
-			if(sv.size() != 0)
+			if(starV.size() != 0)
 			{
-				for(int i=0;i<se.size();i++)
-				{ //쨘짚횇횒횇짤짹창쨍쨍횇짯
-					Point sp = sv.get(i); // 쨘짚횇횒째짧�쨩짼짢쨀쨩쨈횢
-					Point ep = se.get(i);	
-					g.drawRect(sp.x, sp.y, ep.x-sp.x, ep.y-sp.y);//짹횞쨍짰쨈횢
+				for(int i=0;i<endV.size();i++)  // 벡터에 저장된 각 사각형을 매번 그림
+				{
+					Point sp = starV.get(i);
+					Point ep = endV.get(i);	
+					g.drawRect(sp.x, sp.y, ep.x-sp.x, ep.y-sp.y);
 				}
 			}
 			if(startP != null)
@@ -64,24 +59,72 @@ public class SqDrawEx extends JFrame
 			public void mousePressed(MouseEvent e)
 			{
 				startP = e.getPoint();
-				sv.add(e.getPoint()); // 횇짭쨍짱횉횗쨘횓쨘횖�쨩 쩍횄�횤횁징�쨍쨌횓
+				
 
-				k++;
+				for(int i=0;i<endV.size();i++)  // 벡터에 저장된 각 사각형을 비교
+				{
+					 // 사각형 안쪽에 클릭하면 
+					if(startP.x>=starV.get(i).x && startP.x<=endV.get(i).x) // x값 비교
+					{
+						if(startP.y>=starV.get(i).y && endP.x<=endV.get(i).y) // y값 비교
+						{
+							moveSq = true;
+							v=i;
+							break;
+						}
+						
+					}
+					else
+					{
+						starV.add(e.getPoint()); // 쿨릭한 부분을 시작점으로 추가
+						break;
+					}						
+				}
 				
 				
 			}
 			public void mouseReleased(MouseEvent e)
 			{
-				se.add(e.getPoint()); // 쨉책쨌징짹횞 횉횗쨘횓쨘횖�쨩 횁쩐쨌찼횁징�쨍쨌횓
-				endP = e.getPoint();
-				box[k] = new Rectangle(startP.x, startP.y, endP.x-startP.x, endP.y-startP.y);
-				repaint(); // 쨈횢쩍횄짹횞쨌횁쨋처
+
+				if(moveSq)
+				{
+					tempP = e.getPoint();
+					tempP.x = starV.get(v).x + (tempP.x-startP.x); // 현재 백터 x축 + (x축 이동한 거리)
+					tempP.y = starV.get(v).y + (tempP.y-startP.y); // y축 이동한 거리
+					
+
+					starV.add(v,tempP);
+					moveSq=false;
+
+				}
+				else
+				{
+					endV.add(e.getPoint()); // 쨉책쨌징짹횞 횉횗쨘횓쨘횖�쨩 횁쩐쨌찼횁징�쨍쨌횓
+					endP = e.getPoint();
+					 // 쨈횢쩍횄짹횞쨌횁쨋처
+				}
+				repaint();
+
 			}
 			
 			public void mouseDragged(MouseEvent e)
 			{
-				endP = e.getPoint();
+				if(moveSq)
+				{
+					tempP = e.getPoint();
+					tempP.x = starV.get(v).x + (tempP.x-startP.x); // 현재 백터 x축 + (x축 이동한 거리)
+					tempP.y = starV.get(v).y + (tempP.y-startP.y); // y축 이동한 거리
+					
+
+					starV.add(v,tempP);
+					moveSq=false;
+				}
+				else
+				{
+					endP = e.getPoint();
+				}
 				repaint();
+
 			}
 			
 			public void mouseMoved(MouseEvent e)
