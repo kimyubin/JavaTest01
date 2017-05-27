129c1a23ac5bc92052d56bc70e537d175bf5e130
김유빈 <1rladbqls@gmail.com>
2017-05-28 01:04:41 +0900

    이동 완성

--- kimtest/src/RectangleMove.java
+++ kimtest/src/RectangleMove.java
@@ -3,88 +3,136 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.Vector;
 
-class RectangleMove extends JComponent implements MouseListener, MouseMotionListener
+public class RectangleMove extends JFrame 
 {
-	//사각형영역
-	Rectangle box;
-
-	//마우스 드래그 체크
-	boolean isDragged;
-
 	//마우스 오프셋좌표
-	int offX, offY;
-
-	Point startP=null;
-	Point endP=null;
-	Point tempP=null;
-
-	public RectangleMove(){
-
-		//사각형영역 정의(100x80 size)
-
-		box = new Rectangle(0,0,100,80);
-
-		//현재 드래그 상태 저장
-		isDragged = false;
-
-		//마우스 리스너 등록
-		addMouseListener(this);
-		addMouseMotionListener(this);
-
-	}
-
-	//컴포넌트 페인팅
-	public void paintComponent(Graphics g){
-		
-		//사각형 그릴 색상 설정
-		g.setColor(Color.red);
-
-		//사각형 그림
-		g.drawRect(box.x,box.y,box.width,box.height);
-
-		//사각형을 이동하기 위하여 사각형의 x,y 좌표와 사각형 내 클릭한 마우스의 좌표가 필요하다
-
-	}
-
- 	public void mousePressed(MouseEvent e){
-		//사각형 안에서만 이벤트 작업 설정
-		if(box.contains(new Point(e.getX(),e.getY()))){
-			//#1 마우스 버튼 누름
-			//사각형내 마우스 클릭 상대 좌표를 구함
-			//현재 마우스 스크린 좌표에서 사각형 위치 좌표의 차이를 구함
-			offX = e.getX() - box.x;
-			offY = e.getY() - box.y;
-			
-			//드래그 시작을 표시
-			isDragged = true;
-
-		}
-	}
-	public void mouseReleased(MouseEvent e){
-		//마우스 버튼이 릴리즈되면 드래그 모드 종료
-		isDragged = false;
-	}
-	public void mouseDragged(MouseEvent e){
-		
-		//드래그 모드인 경우에만 사각형 이동시킴
-		if(isDragged){
-			box.x = e.getX() - offX;
-			box.y = e.getY() - offY;
-		}
-		repaint();
+	public int offX, offY;
+	public int afterStartX, afterStartY, afterEndX,afterEndY;
+	public boolean isDragged = false;
+	int BoxNum = 2;
 	
+	
+	public	Rectangle rec;
+
+	public	Point startP;
+	public	Point endP;
+	public	Point moveP;
+
+	public RectangleMove()
+	{
+		setContentPane(new MoveSq());
+		setSize(500,500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		startP = new Point();
+		endP = new Point();
+		moveP = new Point();
 	}
-	public void mouseMoved(MouseEvent e){}
-	public void mouseClicked(MouseEvent e){}
-	public void mouseEntered(MouseEvent e){}
-	public void mouseExited(MouseEvent e){}
 
-	//프로그램 실행
-	public static void main(String[] args){
 
-		JFrame f = new JFrame();
-		f.setBounds(0,0,300,300);
-		f.add(new RectangleMove());
-		f.setVisible(true);
+
+	
+	class MoveSq extends JPanel
+	{
+		
+		Vector<Point> startV = new Vector<Point>(); // 시작점
+		Vector<Point> endV = new Vector<Point>(); // 끝
+		
+
+		Point A1s = new Point(0,0);
+		Point A1e = new Point(100,100);
+		Point A2s = new Point(0,150);
+		Point A2e = new Point(100,250);
+		Point A3s = new Point(150,0);
+		Point A3e = new Point(250,100);
+
+
+		public MoveSq()
+		{
+			MouseListen ml = new MouseListen();
+			
+			this.addMouseListener(ml); 
+			this.addMouseMotionListener(ml);
+			//박스 0
+			startV.add(A1s);
+			endV.add(A1e);
+
+			//박스 1
+			startV.add(A2s);
+			endV.add(A2e);
+
+			//박스 2
+			startV.add(A3s);
+			endV.add(A3e);
+		}
+		
+
+
+		public void paintComponent(Graphics g)
+		{
+			super.paintComponent(g); 	
+
+			for(int i=0;i<startV.size();i++)  // 벡터에 저장된 각 사각형을 매번 그림
+				{
+					Point sp = startV.get(i);
+					Point ep = endV.get(i);	
+					g.drawRect(sp.x, sp.y, ep.x-sp.x, ep.y-sp.y);
+				}									
+		}
+		
+		class MouseListen extends MouseAdapter implements MouseMotionListener
+		{
+			public void mousePressed(MouseEvent e)
+			{
+				rec = TransPoint.pointToRec(startV.get(BoxNum),endV.get(BoxNum));
+				if(rec.contains(new Point(e.getX(),e.getY())))
+				{
+					//상대 위치 저장
+					moveP.x=e.getX() - rec.x;
+					moveP.y=e.getY() - rec.y;
+			
+					//드래그 시작을 표시
+					isDragged = true;
+				}
+			}
+
+
+			
+			public void mouseReleased(MouseEvent e)
+			{
+				//마우스 버튼이 릴리즈되면 드래그 모드 종료
+				isDragged = false;		
+			}
+			
+			public void mouseDragged(MouseEvent e)
+			{
+				//드래그 모드인 경우에만 사각형 이동시킴
+				if(isDragged)
+				{
+					rec.x = e.getX() - moveP.x;
+					rec.y = e.getY() - moveP.y;
+
+					startP=TransPoint.RecToStartPoint(rec);
+					endP=TransPoint.RecToEndPoint(rec);
+
+									
+					startV.setElementAt(startP,BoxNum);
+					endV.setElementAt(endP,BoxNum);
+						
+				}
+				repaint();
+			}
+			
+			public void mouseMoved(MouseEvent e){}
+			public void mouseClicked(MouseEvent e){}
+			public void mouseEntered(MouseEvent e){}
+			public void mouseExited(MouseEvent e){}
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		new RectangleMove();
 	}
 }
+
