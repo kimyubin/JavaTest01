4007fa254d6482d6e643c157e1677bbc6c8e4873
김유빈 <1rladbqls@gmail.com>
2017-05-27 22:28:24 +0900

    그림 3개 출력

--- kimtest/src/RectangleMove03.java
+++ kimtest/src/RectangleMove03.java
@@ -6,44 +6,48 @@ import java.util.Vector;
 public class RectangleMove03 extends JFrame 
 {
 	//마우스 오프셋좌표
-	int offX, offY;
-	boolean isDragged = false;
+	public int offX, offY;
+	public int afterStartX, afterStartY, afterEndX,afterEndY;
+	public boolean isDragged = false;
+		
+	public	Rectangle rec;
+
+	public	Point startP;
+	public	Point endP;
+	public	Point moveP;
+
 	public RectangleMove03()
 	{
-		setContentPane(new DrawSq());
-		setSize(300,300);
+		setContentPane(new MoveSq());
+		setSize(500,500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		startP = new Point();
+		endP = new Point();
+		moveP = new Point();
 	}
 
-	public static Rectangle pointToRec(int sx, int sy, int ex, int ey){ //좌표 입력하면 사각형으로 만들어줌
-		return new Rectangle(sx,sy,ex-sx,ey-sy);
-	} 
-	public static Rectangle pointToRec(Point start, Point end){ //좌표 입력하면 사각형으로 만들어줌
-		return new Rectangle(start.x,start.y,end.x-start.x,end.y-start.y);
-	} 
 
-	public Point RecToEndPoint(Rectangle a){ //사각형 입력하면 끝 좌표 만들어줌
-		return new Point(a.x+a.width,a.y+a.height);
-	} 
 
 	
-	class DrawSq extends JPanel
+	class MoveSq extends JPanel
 	{
 		
-		Point startP= null;
-		Point endP=null;
-		Point tempP=null;
-		
 		Vector<Point> startV = new Vector<Point>(); // 시작점
-		Vector<Point> endV = new Vector<Point>(); // 끝점
+		Vector<Point> endV = new Vector<Point>(); // 끝
+		
 
-	
+		Point A1s = new Point(0,0);
+		Point A1e = new Point(100,100);
+		Point A2s = new Point(0,150);
+		Point A2e = new Point(100,250);
+		Point A3s = new Point(150,0);
+		Point A3e = new Point(250,100);
 
 
-		public DrawSq()
+		public MoveSq()
 		{
-			MyMouseListener ml = new MyMouseListener();
+			MouseListen ml = new MouseListen();
 			
 			this.addMouseListener(ml); 
 			this.addMouseMotionListener(ml);
@@ -56,59 +60,43 @@ public class RectangleMove03 extends JFrame
 			super.paintComponent(g); 	
 
 			//박스 0
-			startP.move(0,0);
-			endP.move(100,100);
-			startV.add(startP);
-			endV.add(endP);
+			startV.add(A1s);
+			endV.add(A1e);
 
 			//박스 1
-			startP.move(101,101);
-			endP.move(201,201);
-			startV.add(startP);
-			endV.add(endP);
+			startV.add(A2s);
+			endV.add(A2e);
 
 			//박스 2
-			startP.move(150,0);
-			endP.move(250,100);
-			startV.add(startP);
-			endV.add(endP);
+			startV.add(A3s);
+			endV.add(A3e);
 
-		
-
-
-
-			if(startV.size() != 0)
-			{
-				for(int i=0;i<endV.size();i++)  // 벡터에 저장된 각 사각형을 매번 그림
+			for(int i=0;i<startV.size();i++)  // 벡터에 저장된 각 사각형을 매번 그림
 				{
 					Point sp = startV.get(i);
 					Point ep = endV.get(i);	
 					g.drawRect(sp.x, sp.y, ep.x-sp.x, ep.y-sp.y);
-				}
-			}
-							
+				}									
 		}
 		
-		class MyMouseListener extends MouseAdapter implements MouseMotionListener
+		class MouseListen extends MouseAdapter implements MouseMotionListener
 		{
-			Rectangle rec;
 			public void mousePressed(MouseEvent e)
 			{
-				rec = RectangleMove03.pointToRec(startV.get(1),endV.get(1));
+				rec = TransPoint.pointToRec(startV.get(0),endV.get(0));
 				if(rec.contains(new Point(e.getX(),e.getY())))
 				{
-					//사각형내 마우스 클릭 상대 좌표를 구함
-					//현재 마우스 스크린 좌표에서 사각형 위치 좌표의 차이를 구함
-					offX = e.getX() - rec.x;
-					offY = e.getY() - rec.y;
-					
+					//현재 위치 저장 
+					moveP.x=e.getX() - rec.x;
+					moveP.y=e.getY() - rec.y;
+			
 					//드래그 시작을 표시
 					isDragged = true;
 				}
 			}
 
 
-			}
+			
 			public void mouseReleased(MouseEvent e)
 			{
 				//마우스 버튼이 릴리즈되면 드래그 모드 종료
@@ -120,24 +108,66 @@ public class RectangleMove03 extends JFrame
 				//드래그 모드인 경우에만 사각형 이동시킴
 				if(isDragged)
 				{
-					RectangleMove03.pointToRec(startV.get(1),endV.get(1)).insertElementAt
-
-					startV.add(e.getX() - offX);
+					/*
 					rec.x = e.getX() - offX;
 					rec.y = e.getY() - offY;
+
+					startP=TransPoint.RecToStartPoint(rec);
+					endP=TransPoint.RecToEndPoint(rec);
+
+				
+					
+					startV.setElementAt(startP,0);
+					endV.setElementAt(endP,0);
+				*/
+
+					afterStartX = startV.get(1).x+(e.getX() - moveP.x); // 저장된 X 좌표 + (현재 위치 - 클릭 위치)(이동 거리) = 이동 후 좌표
+					afterStartY = startV.get(1).y+(e.getY() - moveP.y);
+					afterEndX = endV.get(1).x+(e.getX() - moveP.x); 
+					afterEndY = endV.get(1).y+(e.getX() - moveP.x);
+
+					startP.move(afterStartX,afterStartY);
+					endP.move(afterEndX,afterEndY);
+					
+					startV.setElementAt(startP, 1);
+					endV.setElementAt(endP, 1);
+
 				}
-				repaint();				
+
 			}
 			
-			public void mouseMoved(MouseEvent e)
-			{
-				
-			}
+			public void mouseMoved(MouseEvent e){}
+			public void mouseClicked(MouseEvent e){}
+			public void mouseEntered(MouseEvent e){}
+			public void mouseExited(MouseEvent e){}
 		}
 	}
 	
 	public static void main(String[] args) 
 	{
-		new SqDrawEx();
+		new RectangleMove03();
 	}
+}
+
+
+class TransPoint // 유틸
+{
+	//좌표 입력하면 사각형으로 만들어줌
+	public static Rectangle pointToRec(int sx, int sy, int ex, int ey){ 
+		return new Rectangle(sx,sy,ex-sx,ey-sy);
+	} 
+	//포인트 입력하면 사각형으로 만들어줌
+	public static Rectangle pointToRec(Point start, Point end){ 
+		return new Rectangle(start.x,start.y,end.x-start.x,end.y-start.y);
+	} 
+
+	//사각형 입력하면 시작 좌표 만들어줌
+	public static Point RecToStartPoint(Rectangle a){ 
+		return new Point(a.x,a.y);
+	} 
+
+	 //사각형 입력하면 끝 좌표 만들어줌
+	public static Point RecToEndPoint(Rectangle a){
+		return new Point(a.x+a.width,a.y+a.height);
+	} 
 }
--- kimtest/src/SqDrawEx.java
+++ kimtest/src/SqDrawEx.java
@@ -10,28 +10,27 @@ public class SqDrawEx extends JFrame
 
 	public SqDrawEx()
 	{
-		setContentPane(new MyPanel());
+		setContentPane(new DrawSq());
 		setSize(300,300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
 	
-	class MyPanel extends JPanel
+	class DrawSq extends JPanel
 	{
 		
 		Point startP=null;
 		Point endP=null;
 		Point tempP=null;
 		
-		Vector<Point> starV = new Vector<Point>(); // 시작점
+		Vector<Point> startV = new Vector<Point>(); // 시작점
 		Vector<Point> endV = new Vector<Point>(); // 끝점
 
-		public MyPanel()
+		public DrawSq()
 		{
-			//쨍짰쩍쨘쨀횎쨍짝 째첩횇챘�쨍쨌횓횉횠쩐횩  쨘짱쩌철쨉챕�횑 째첩�짱쨉횊쨈횢.
 			MyMouseListener ml = new MyMouseListener();
 			
-			this.addMouseListener(ml); // 쨍짰쩍쨘쨀횎
+			this.addMouseListener(ml); // 
 			this.addMouseMotionListener(ml);
 		}
 		
@@ -41,11 +40,11 @@ public class SqDrawEx extends JFrame
 			super.paintComponent(g); 			
 
 
-			if(starV.size() != 0)
+			if(startV.size() != 0)
 			{
 				for(int i=0;i<endV.size();i++)  // 벡터에 저장된 각 사각형을 매번 그림
 				{
-					Point sp = starV.get(i);
+					Point sp = startV.get(i);
 					Point ep = endV.get(i);	
 					g.drawRect(sp.x, sp.y, ep.x-sp.x, ep.y-sp.y);
 				}
@@ -58,57 +57,21 @@ public class SqDrawEx extends JFrame
 		class MyMouseListener extends MouseAdapter implements MouseMotionListener{
 			public void mousePressed(MouseEvent e)
 			{
-				startP = e.getPoint();
-				
-
-				for(int i=0;i<endV.size();i++)  // 벡터에 저장된 각 사각형을 비교
-				{
-					 // 사각형 안쪽에 클릭하면 
-					if(startP.x>=starV.get(i).x && endP.x<=endV.get(i).x) // x값 비교
-					{
-						if(startP.y>=starV.get(i).y && endP.x<=endV.get(i).y) // y값 비교
-						{
-							moveSq = true;
-							v=i;
-							break;
-						}
-						
-					}
-					else
-					{
-						starV.add(e.getPoint()); // 쿨릭한 부분을 시작점으로 추가
-						break;
-					}						
-				}
-				
+				startP = e.getPoint();		
+				startV.add(e.getPoint());
 				
 			}
 			public void mouseReleased(MouseEvent e)
 			{
-				moveSq = false;
-				endV.add(e.getPoint()); // 쨉책쨌징짹횞 횉횗쨘횓쨘횖�쨩 횁쩐쨌찼횁징�쨍쨌횓
+				endV.add(e.getPoint());
 				endP = e.getPoint();
-					 // 쨈횢쩍횄짹횞쨌횁쨋처
-				
 				repaint();
 
 			}
 			
 			public void mouseDragged(MouseEvent e)
 			{
-				if(moveSq)
-				{
-					tempP = e.getPoint();
-					tempP.x = starV.get(v).x + (tempP.x-startP.x); // 현재 백터 x축 + (x축 이동한 거리)
-					tempP.y = starV.get(v).y + (tempP.y-startP.y); // y축 이동한 거리
-					
-
-					starV.add(v,tempP);
-				}
-				else
-				{
-					endP = e.getPoint();
-				}
+				endP = e.getPoint();
 				repaint();
 
 			}
